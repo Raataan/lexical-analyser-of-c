2dbe9117f06ce29bd88ee93fa386f9005728b7bd
Raataan <1262343019@qq.com>
2020-10-07 18:50:57 +0800

    git pull

--- lexical_analyser_of_c.cpp
+++ lexical_analyser_of_c.cpp
@@ -1,52 +1,95 @@
-#include <cmath>
-#include <cstring>
 #include <fstream>
 #include <iostream>
+#include <sstream>
+#include <string>
+
 using namespace std;
-bool is_keyword(char buffer[])
-{
-    char keywords[32][10] = {
-        "auto", "break", "case", "char", "const", "continue",
-        "default", "do", "double", "else", "enum", "extern",
-        "float", "for", "goto", "if", "int", "long",
-        "register", "return", "short", "signed", "sizeof", "static",
-        "struct", "switch", "typedef", "union", "unsigned", "void",
-        "volatile", "while"};
-    for (int i = 0; i < 32; i++)
-    {
-        if (strcmp(keywords[i], buffer) == 0)
-            return true;
-    }
-    return false;
-}
-bool is_operator(char buffer[])
-{
-    char keywords[17][3] = {"+", "-", "*", "/", "<<", ">>", "%",">","<","=","==","+=","-=","/=","*=","++","--"};
-    for (int i = 0; i < 15; i++)
-    {
-        if (strcmp(keywords[i], buffer) == 0)
-            return true;
-    }
-    return false;
-}
+void next();
+string read_file_to_string();
+string code = read_file_to_string();
+string::iterator iter = code.begin();
+char *src = (char *)code.c_str();
+char token;
+int token_val, line = 1;
+ofstream fout;
 int main()
 {
-    ifstream fin("program.txt");
-    ofstream fout("mark.txt");
-    string line_now;
-    if(!fin.is_open())
-        cout << "Error: Can\'t open the file. ";
-    else
+    fout.open("mark.txt", ios::out | ios::trunc);
+    while ((token = *iter))
     {
-        while(fin.good() && !fin.eof())
+        if (token == '#')
         {
-            line_now.clear();
-            getline(fin, line_now);
-            cout << line_now << endl;
+            while (*iter != '\n')
+                iter++;
+            token = *iter;
         }
+        if (token == '\n')
+            line++;
+        next();
+        iter++;
     }
-
-    fin.close();
+    cout << line;
     fout.close();
     return 0;
+}
+string read_file_to_string()
+{
+    ifstream fs("program.c", ios::binary);
+    stringstream ss;
+    ss << fs.rdbuf();
+    fs.close();
+    string str = ss.str(); //read into string
+    return str;
+}
+void next()
+{
+    if (token >= '0' && token <= '9')
+    {
+        // parse number, three kinds: dec(123) hex(0x123) oct(017)
+        token_val = token - '0';
+        iter++;
+        if (token_val > 0)
+        {
+            // dec, starts with [1-9]
+            while (*iter >= '0' && *iter <= '9')
+            {
+                token_val = token_val * 10 + *iter++ - '0';
+            }
+        }
+        else
+        {
+            // starts with number 0
+            if (*iter == 'x' || *iter == 'X')
+            {
+                //hex
+                token = *++iter;
+                while ((token >= '0' && token <= '9') || (token >= 'a' && token <= 'f') || (token >= 'A' && token <= 'F'))
+                {
+                    token_val = token_val * 16 + (token & 15) + (token >= 'A' ? 9 : 0);
+                    token = *++iter;
+                }
+            }
+            else
+            {
+                // oct
+                while (*iter >= '0' && *iter <= '7')
+                {
+                    token_val = token_val * 8 + *iter++ - '0';
+                }
+            }
+        }
+        fout << "< Number, Line=" << line << ", Value=" << token_val<<" >"<<endl;
+        return;
+    }
+    else if (token == '"') //string
+    {
+        string now;
+        while (*++iter != '"')
+        {
+            if (*iter == '\n')
+                line++;
+            now.push_back(*iter);
+        }
+        fout << "< String, Line=" << line << ", Value=" << now<<" >"<<endl;
+    }
 }
